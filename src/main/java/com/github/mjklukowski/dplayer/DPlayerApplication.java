35e4fa1af5ba9e2d560b84b67dfc1c4a29cf2d9a
Michał Klukowski <michal.klukowski@talan.com>
2023-08-05 20:14:07 +0200

    Initialize Spring Boot application

---
+++ src/main/java/com/github/mjklukowski/dplayer/DPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.github.mjklukowski.dplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mjklukowski/dplayer/DplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mjklukowski.dplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
