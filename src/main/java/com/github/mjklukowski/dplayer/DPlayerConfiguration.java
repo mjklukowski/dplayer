878fee886333a57e79be36ac339a00c70f233378
Michał Klukowski <michal.klukowski@talan.com>
2023-08-05 20:17:46 +0200

    Login to Discord

---
+++ src/main/java/com/github/mjklukowski/dplayer/DPlayerConfiguration.java
@@ -0,0 +1,22 @@
+package com.github.mjklukowski.dplayer;
+
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DPlayerConfiguration {
+
+    @Value("${discord.token}")
+    private String token;
+
+    @Bean
+    public GatewayDiscordClient createDiscordClient() {
+        return DiscordClientBuilder.create(token)
+                .build()
+                .login()
+                .block();
+    }
+}
