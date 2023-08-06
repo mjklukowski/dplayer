559cf1941b5839bbbe28d57942765bec1c14a1d4
Michał Klukowski <michal.klukowski@talan.com>
2023-08-06 16:15:52 +0200

    Disconnect bot on stop action

--- src/main/java/com/github/mjklukowski/dplayer/player/services/DiscordPlaybackService.java
+++ src/main/java/com/github/mjklukowski/dplayer/player/services/DiscordPlaybackService.java
@@ -6,6 +6,7 @@ import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.channel.VoiceChannel;
 import discord4j.core.spec.VoiceChannelJoinSpec;
 import discord4j.voice.AudioProvider;
+import discord4j.voice.VoiceConnection;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -61,6 +62,8 @@ public class DiscordPlaybackService implements PlaybackService {
 
     @Override
     public void stop(VoiceChannel channel) {
-
+        channel.getVoiceConnection()
+                .flatMap(VoiceConnection::disconnect)
+                .block();
     }
 }
